314916ddc9191d2d7efbdb035df6fc88da708983
georgepauly <georgepauly1995@gmail.com>
2025-03-19 09:34:50 +0530

    Added the pause button and  changed alignment of elements

--- timer.js
+++ timer.js
@@ -1,30 +1,36 @@
 let session_duration = document.getElementsByClassName("time")[0].innerHTML;
 let minutes_duration = session_duration.split(":")[0]
 let seconds_duration = session_duration.split(":")[1]
-let counting_down = false;
-document.getElementsByClassName("start")[0].addEventListener("click",count_down);
+
+
+document.getElementsByClassName("start")[0].addEventListener("click", count_down, {once: true});
 
 function count_down(){
-    if (counting_down) return
-    document.getElementsByClassName("start")[0].innerHTML = "PAUSE"
-    counting_down = true
+    document.getElementsByClassName("start")[0].innerHTML = "PAUSE";
+    document.getElementsByClassName("start")[0].addEventListener("click", pause, {once: true});
     const interval = setInterval(timer,1000);
+
+    function pause(){
+        clearInterval(interval)
+        document.getElementsByClassName("start")[0].innerHTML = "START";
+        document.getElementsByClassName("start")[0].addEventListener("click", count_down, {once: true});
+    }
     function timer(){
-        if (seconds_duration==0){
-            minutes_duration--
+        if (seconds_duration == 0){
+            minutes_duration --
         }
-        seconds_duration--
-        seconds_duration = (seconds_duration+60)%60
-        if (seconds_duration<10 && seconds_duration.toString.length==1){
-            seconds_duration = "0"+seconds_duration
+        seconds_duration --
+        seconds_duration = (seconds_duration + 60) % 60
+        if (seconds_duration < 10 && seconds_duration.toString.length == 1){
+            seconds_duration = "0" + seconds_duration
         }
-        if (minutes_duration<10 && minutes_duration.toString.length==1){
-            minutes_duration = "0"+minutes_duration
+        if (minutes_duration < 10 && minutes_duration.toString.length == 1){
+            minutes_duration = "0" + minutes_duration
         }
-        document.getElementsByTagName("h1")[0].innerHTML = minutes_duration+":"+seconds_duration;
-        document.getElementsByTagName("title")[0].innerHTML = minutes_duration+":"+seconds_duration+" - "+"Time for Focus!";
+        document.getElementsByTagName("h1")[0].innerHTML = minutes_duration + ":" + seconds_duration;
+        document.getElementsByTagName("title")[0].innerHTML = minutes_duration + ":" + seconds_duration + " - " + "Time to Focus!";
         if (minutes_duration <= 0 && seconds_duration == 0){
             clearInterval(interval)
-        }                 
+        }
     }
 }
