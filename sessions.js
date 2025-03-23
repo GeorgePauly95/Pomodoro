60e14300fb806b354a9623bd85e9872c37863f3b
georgepauly <georgepauly1995@gmail.com>
2025-03-23 23:14:15 +0530

    Added functionality to choose to move between sessions automatically

--- sessions.js
+++ sessions.js
@@ -1,42 +1,15 @@
-document.getElementById("pomodoro-session").addEventListener("click", pomodoro_session);
-document.getElementById("short-break-session").addEventListener("click", short_break_session);
-document.getElementById("long-break-session").addEventListener("click", long_break_session);
-function pomodoro_session(){
-    document.body.style.backgroundColor = "#3A5BDC";
-    document.getElementById("session-message").innerText = "Time to focus!";
-    document.getElementById("pomodoro-session").style.boxShadow = "10px 5px 5px black";
-    document.getElementById("short-break-session").style.boxShadow = "";
-    document.getElementById("long-break-session").style.boxShadow = "";
-    setPomodoroTime()
-    clearInterval()
-    if (document.getElementById("time").innerText == ":00"){
-        document.getElementById("time").innerText = `${document.querySelector("input[id='pomodoro-length']").placeholder}:00`;
-
-    }
-}
-function short_break_session(){    
-    document.body.style.backgroundColor = "#2A9D8F";
-    document.getElementById("session-message").innerText = "Time for a break!";
-    document.getElementById("pomodoro-session").style.boxShadow = "";
-    document.getElementById("short-break-session").style.boxShadow = "10px 5px 5px black";
-    document.getElementById("long-break-session").style.boxShadow = "";
-    setShortBreakTime()
-    clearInterval()
-    if (document.getElementById("time").innerText == ":00"){
-        document.getElementById("time").innerText = `${document.querySelector("input[id='short-break-length']").placeholder}:00`;
-
-    }    
-}
-function long_break_session(){
-    document.body.style.backgroundColor = "#A37ACE";
-    document.getElementById("session-message").innerText = "Extended break time!";
-    document.getElementById("pomodoro-session").style.boxShadow = "";
-    document.getElementById("short-break-session").style.boxShadow = "";
-    document.getElementById("long-break-session").style.boxShadow = "10px 5px 5px black";
-    setLongBreakTime()
-    clearInterval()
-    if (document.getElementById("time").innerText == ":00"){
-        document.getElementById("time").innerText = `${document.querySelector("input[id='long-break-length']").placeholder}:00`;
-
+document.getElementById("pomodoro-session").addEventListener("click", () => template_session("#3A5BDC", "Time to focus!", ["10px 5px 5px black", "", ""], setPomodoroTime));
+document.getElementById("short-break-session").addEventListener("click", () => template_session("#2A9D8F", "Time for a break!", ["", "10px 5px 5px black", ""], setShortBreakTime));
+document.getElementById("long-break-session").addEventListener("click", () => template_session("#A37ACE", "Extended break time!", ["", "", "10px 5px 5px black"], setLongBreakTime));
+document.getElementById("pomodoro-session").click()
+function template_session(color, session_message, [pomodoro_shadow, short_break_shadow, long_break_shadow], setTime) {
+    document.body.style.backgroundColor = color;
+    document.getElementById("session-message").innerText = session_message;
+    document.getElementById("pomodoro-session").style.boxShadow = pomodoro_shadow;
+    document.getElementById("short-break-session").style.boxShadow = short_break_shadow;
+    document.getElementById("long-break-session").style.boxShadow = long_break_shadow;
+    setTime();
+    if (document.getElementById("start").innerText == "PAUSE") {
+        document.getElementById("start").click()
     }
 }
--- settings.js
+++ settings.js
@@ -1,27 +1,33 @@
 const settings_button = document.getElementById("settings").addEventListener("click", showSettingsDialog);
 const dialog = document.getElementById("settings-dialog");
-function showSettingsDialog(){
+function showSettingsDialog() {
+    document.getElementById("settings").style.boxShadow = "10px 5px 5px black";
     dialog.showModal();
 }
+
 const settings_close_button = document.getElementById("settings-close").addEventListener("click", closeSettingsDialog);
-function closeSettingsDialog(){
+function closeSettingsDialog() {
+    document.getElementById("settings").style.boxShadow = "";
     dialog.close();
 }
 
 document.getElementById("pomodoro-length").addEventListener("input", setPomodoroTime);
-function setPomodoroTime(){
+function setPomodoroTime() {
     const pomodoro_time = document.getElementById("pomodoro-length").value;
-    document.getElementById("time").innerText = `${pomodoro_time}:00`
+    document.getElementById("pomodoro-session").click();
+    document.getElementById("time").innerText = `${pomodoro_time}:00`;
 }
 
 document.getElementById("short-break-length").addEventListener("input", setShortBreakTime);
-function setShortBreakTime(){
+function setShortBreakTime() {
     const short_break_time = document.getElementById("short-break-length").value;
-    document.getElementById("time").innerText = `${short_break_time}:00`
+    document.getElementById("short-break-session").click();
+    document.getElementById("time").innerText = `${short_break_time}:00`;
 }
 
 document.getElementById("long-break-length").addEventListener("input", setLongBreakTime);
-function setLongBreakTime(){
+function setLongBreakTime() {
     const long_break_time = document.getElementById("long-break-length").value;
-    document.getElementById("time").innerText = `${long_break_time}:00`
+    document.getElementById("long-break-session").click();
+    document.getElementById("time").innerText = `${long_break_time}:00`;
 }
