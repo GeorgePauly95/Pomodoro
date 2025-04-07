abfa129ef82fea9a742a9ac176d8e4d65a1ab491
georgepauly <georgepauly1995@gmail.com>
2025-04-07 10:20:30 +0530

    Reduced the number of POST Requests
    - Removed 3 of the 4 routes that sent session lengths
    - Renamed existing routes and functions

--- main.py
+++ main.py
@@ -2,51 +2,28 @@ from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
 pomodoro_config = {
-    'pomodoro_length': 65,    
-    'short_break_length': 35,
-    'long_break_length': 95,
-    'long_break_count':15}
-
+    'pomodoro_length': 35,    
+    'short_break_length': 5,
+    'long_break_length': 15,
+    'long_break_count':5}
 
 @app.route('/')
 def main_func():
     return render_template("index.html")
 
-@app.route('/check')
-def sub_func():
+@app.route('/loadConfig')
+def load_config():
     return jsonify(pomodoro_config)
 
-@app.route('/test1', methods=['POST','GET'])
-def modify_pomodoro_config1():
+@app.route('/settings', methods=['POST'])
+def modify_pomodoro_config():
     output = request.get_json()
     pomodoro_config['pomodoro_length'] = output.get("pomodoro_length")
-    print(f"This is the new data in backend: {pomodoro_config}\n")
-    return jsonify(pomodoro_config)
-        
-@app.route('/test2', methods=['POST'])
-def modify_pomodoro_config2():
-    output = request.get_json()
     pomodoro_config['short_break_length'] = output.get("short_break_length")
+    pomodoro_config['long_break_length'] = output.get("long_break_length")
+    pomodoro_config['long_break_count'] = output.get("long_break_count")
     print(f"This is the new data in backend: {pomodoro_config}\n")
     return jsonify(pomodoro_config)
-        
-@app.route('/test3', methods=['POST'])
-def modify_pomodoro_config3():
-    if request.method == 'POST':
-        output = request.get_json()
-        pomodoro_config['long_break_length'] = output.get("long_break_length")
-        print(f"This is the new data in backend: {pomodoro_config}\n")
-        return jsonify(pomodoro_config)
-        
-@app.route('/test4', methods=['POST'])
-def modify_pomodoro_config4():
-    if request.method == 'POST':
-        output = request.get_json()
-        pomodoro_config['long_break_count'] = output.get("long_break_count")
-        print(f"This is the new data in backend: {pomodoro_config}\n")
-        return jsonify(pomodoro_config)
-  
-
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/settings.js
+++ static/settings.js
@@ -13,15 +13,15 @@ const settings_close_button = document.getElementById("settings-close").addEvent
 function closeSettingsDialog() {
     document.getElementById("settings").style.boxShadow = "";
     dialog.close();
+    fetch('/settings', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ "pomodoro_length": document.getElementById("pomodoro-length").value, 'short_break_length': document.getElementById("short-break-length").value, 'long_break_length': document.getElementById("long-break-length").value, 'long_break_count': document.getElementById("long-break-count").value })
+    }).then(output => { output.json(); });
 }
 
 document.getElementById("pomodoro-length").addEventListener("input", setPomodoroTime);
 function setPomodoroTime() {
-    fetch('/test1', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ "pomodoro_length": document.getElementById("pomodoro-length").value })
-    }).then(output => { output.json(); });
     if (document.getElementById("session-message").innerText == "Time to focus!") {
         document.getElementById("time").innerText = `${document.getElementById("pomodoro-length").value}:00`;
     }
@@ -29,11 +29,6 @@ function setPomodoroTime() {
 
 document.getElementById("short-break-length").addEventListener("input", setShortBreakTime);
 function setShortBreakTime() {
-    fetch('/test2', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ 'short_break_length': document.getElementById("short-break-length").value })
-    }).then(output => { output.json(); });
     if (document.getElementById("session-message").innerText == "Time for a break!") {
         document.getElementById("time").innerText = `${document.getElementById("short-break-length").value}:00`;
     }
@@ -41,20 +36,11 @@ function setShortBreakTime() {
 
 document.getElementById("long-break-length").addEventListener("input", setLongBreakTime);
 function setLongBreakTime() {
-    fetch('/test3', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ 'long_break_length': document.getElementById("long-break-length").value })
-    }).then(output => { output.json(); });
     if (document.getElementById("session-message").innerText == "Extended break time!") {
         document.getElementById("time").innerText = `${document.getElementById("long-break-length").value}:00`;
     }
 }
+
 document.getElementById("long-break-count").addEventListener("input", setLongBreakCount);
 function setLongBreakCount() {
-    fetch('/test4', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ 'long_break_count': document.getElementById("long-break-count").value })
-    }).then(output => { output.json(); });
 } 
